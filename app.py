8b023d9f92ccd9a9e4302c2065eb915d4dc63501
kebekchieva <77875041+kebekchieva@users.noreply.github.com>
2021-10-19 14:18:47 +0600

    init

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def homepage():
-    f = open("goods.txt", "r", encoding="utf-8")
+    f = open('goods.txt', "r", encoding="utf-8")
     txt = f.readlines()
     return render_template("index.html", goods=txt)
+
+
+@app.route("/add/", methods=["POST"])
+def add():
+    good = request.form["good"]
+    f = open('goods.txt', 'a+', encoding='utf-8')
+    f.write(good + "\n")
+    f.close()
+    return """
+        <h1>Инвентарь пополнен</h1>
+        <a href="/">Домой</a>
+    """
